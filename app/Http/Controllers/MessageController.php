f3c9b2c1258061a55922658acc9fe5b1dd4e7ee8
Marco 9726 <marcosarra9@gmail.com>
2023-03-23 17:36:14 +0100

    create message crud closed #25

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -8,79 +8,87 @@ use App\Models\Message;
 
 class MessageController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function index()
+	{
+		$messages = Message::all();
+		return view('admin.messages.index', compact('messages'));
+	}
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function create()
+	{
+		$messages = Message::all();
+		return view('messages.create', compact('messages'));
+	}
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\StoreMessageRequest  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(StoreMessageRequest $request)
-    {
-        //
-    }
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @param  \App\Http\Requests\StoreMessageRequest  $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function store(StoreMessageRequest $request)
+	{
+		// Recupero i dati validati dalla richiesta
+		$form_data = $request->validated();
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Message  $message
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Message $message)
-    {
-        //
-    }
+		$newMessage = Message::create($form_data);
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Message  $message
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Message $message)
-    {
-        //
-    }
+		// return redirect()->route('')->with('message', 'Il messaggio è stato inviato correttamente');
+	}
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\UpdateMessageRequest  $request
-     * @param  \App\Models\Message  $message
-     * @return \Illuminate\Http\Response
-     */
-    public function update(UpdateMessageRequest $request, Message $message)
-    {
-        //
-    }
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  \App\Models\Message  $message
+	 * @return \Illuminate\Http\Response
+	 */
+	public function show(Message $message)
+	{
+		//
+	}
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Message  $message
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Message $message)
-    {
-        //
-    }
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  \App\Models\Message  $message
+	 * @return \Illuminate\Http\Response
+	 */
+	public function edit(Message $message)
+	{
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  \App\Http\Requests\UpdateMessageRequest  $request
+	 * @param  \App\Models\Message  $message
+	 * @return \Illuminate\Http\Response
+	 */
+	public function update(UpdateMessageRequest $request, Message $message)
+	{
+		//
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  \App\Models\Message  $message
+	 * @return \Illuminate\Http\Response
+	 */
+	public function destroy(Message $message)
+	{
+		$message->delete();
+
+		return redirect()->route('admin.messages.index')->with('message', 'Messaggio eliminato correttamente');
+	}
 }
