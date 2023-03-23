0c6555408d685d485de2e639df0be926aa50bf77
roberto.boston <roberto.bozzo97@tiscali.it>
2023-03-23 12:36:05 +0100

    added position rcms

--- app/Models/Position.php
+++ app/Models/Position.php
@@ -7,5 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Position extends Model
 {
+    protected $fillable = ['indirizzo','N_civico','Latitudine','Longitudine','città','Nazione','apartment_id'];
     use HasFactory;
+
+    public function apartment()
+    {
+        return $this->belongsTo(Apartment::class);
+    }
 }
