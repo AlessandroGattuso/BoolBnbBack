cda7feb0ff3cefe73b6e950f0bdc6d158a5d1ea9
micheleTummolo <tummolomichele02@gmail.com>
2023-03-23 10:22:26 +0100

    closed #14 created services seeder and fillable

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Service extends Model
 {
+    protected $fillable = ['nome'];
+
     public function apartments(){
         return $this->belongsToMany(Apartment::class);
     }
