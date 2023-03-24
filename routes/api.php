65b03c4158e79a71631f012d4f98cca8e282f70c
Alessandro Gattuso <alessandrogattuso@yahoo.it>
2023-03-24 12:50:23 +0100

    added messages and views controller in api.php

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\MessageController as MessageController;
+use App\Http\Controllers\Api\ViewController as ViewController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,13 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::get('/messages/{apartment}', [MessageController::class, 'index']);
+Route::put('/messages/{apartment}', [MessageController::class, 'store']);
+Route::delete('/messages/{apartment}', [MessageController::class, 'delete']);
+
+Route::get('/views/{apartment}', [ViewController::class, 'index']);
+Route::put('/views/{apartment}', [ViewController::class, 'store']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
