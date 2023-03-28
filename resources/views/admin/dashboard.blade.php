51d204001bfcf6d649fced41f17d4407e1555b62
micheleTummolo <tummolomichele02@gmail.com>
2023-03-28 11:19:09 +0200

    Added delete button

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -15,19 +15,26 @@
                 <h4 class="num-appartamenti">Tutti i tuoi appartamenti:(0)</h4>
             </div>
         </div>
+
+        @if(session('message'))
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                <strong>{{session('message')}}</strong>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        @endif
+
         @if(count($apartments) > 0)
             @foreach ($apartments as $item)
                 <div class="col-12">
                     <div class="my-5 d-md-flex text-lg-left justify-content-lg-start">
                         <img class="rounded" src="{{$item->cover}}" alt="{{$item->slug}}">
-                    </div>
-                    <div class="ms-md-3">
-                        <h5 id="2">ID: {{$item->id}}</h5>
-                        <p>Descrizione: {{$item->descrizione}}</p>
-                        <p>{{$item->slug}}</p>
-                        <a href="{{route('admin.apartments.show',$item->slug)}}" class="eye"><i class="fa-solid fa-eye fa-2x m-2"></i></a>
-                        <a href="{{route('admin.apartments.edit', $item->slug)}}" class="edit"><i class="fa-solid fa-pen-to-square fa-2x m-2"></i></a>
-                        <a href="" class="delete"><i class="fa-solid fa-trash fa-2x m-2"></i></a>
+                        <div class="ms-md-3">
+                            <h5 id="2">ID: {{$item->id}}</h5>
+                            <p>Descrizione: {{$item->descrizione}}</p>
+                            <p>{{$item->slug}}</p>
+                            <a href="{{route('admin.apartments.show',$item->slug)}}" class="eye"><i class="fa-solid fa-eye fa-2x m-2"></i></a>
+                            <a href="{{route('admin.apartments.edit', $item->slug)}}" class="edit"><i class="fa-solid fa-pen-to-square fa-2x m-2"></i></a>
+                        </div>
                     </div>
                 </div>
             @endforeach
