1b3b9bb257790e45192bde86416942e5034931fa
micheleTummolo <tummolomichele02@gmail.com>
2023-03-28 10:54:32 +0200

    Fixed

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -17,19 +17,19 @@
         </div>
         @if(count($apartments) > 0)
             @foreach ($apartments as $item)
-            <div class="col-12">
-                <div class="my-5 d-md-flex text-lg-left justify-content-lg-start">
-                    <img class="rounded" src="{{$item->cover}}" alt="{{$item->slug}}">
+                <div class="col-12">
+                    <div class="my-5 d-md-flex text-lg-left justify-content-lg-start">
+                        <img class="rounded" src="{{$item->cover}}" alt="{{$item->slug}}">
+                    </div>
+                    <div class="ms-md-3">
+                        <h5 id="2">ID: {{$item->id}}</h5>
+                        <p>Descrizione: {{$item->descrizione}}</p>
+                        <p>{{$item->slug}}</p>
+                        <a href="{{route('admin.apartments.show',$item->slug)}}" class="eye"><i class="fa-solid fa-eye fa-2x m-2"></i></a>
+                        <a href="{{route('admin.apartments.edit', $item->slug)}}" class="edit"><i class="fa-solid fa-pen-to-square fa-2x m-2"></i></a>
+                        <a href="" class="delete"><i class="fa-solid fa-trash fa-2x m-2"></i></a>
+                    </div>
                 </div>
-                <div class="ms-md-3">
-                    <h5 id="2">ID: {{$item->id}}</h5>
-                    <p>Descrizione: {{$item->descrizione}}</p>
-                    <p>{{$item->slug}}</p>
-                    <a href="{{route('admin.apartments.show',$item->slug)}}" class="eye"><i class="fa-solid fa-eye fa-2x m-2"></i></a>
-                    <a href="{{route('admin.apartments.edit', $item->slug)}}" class="edit"><i class="fa-solid fa-pen-to-square fa-2x m-2"></i></a>
-                    <a href="" class="delete"><i class="fa-solid fa-trash fa-2x m-2"></i></a>
-                </div>
-            </div>
             @endforeach
         @else
         <div class="pt-5 mt-5 text-center">
