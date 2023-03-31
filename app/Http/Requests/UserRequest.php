4213fe0ab7aac38e159d6824e3304eab11741b89
Alessandro Gattuso <alessandrogattuso@yahoo.it>
2023-03-31 09:56:47 +0200

    added date validation

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -28,7 +28,7 @@ class UserRequest extends FormRequest
         return [
             'nome' => ['nullable','max:255'],
 			'cognome' => ['nullable', 'max:255'],
-			'data_di_nascita' => ['nullable','date'],
+			'data_di_nascita' => ['nullable','date', 'before:-13 years', 'after:1899-12-31'],
 			'email' => ['required', 'string', 'email', 'max:255', 'unique:' . User::class],
 			'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ];
