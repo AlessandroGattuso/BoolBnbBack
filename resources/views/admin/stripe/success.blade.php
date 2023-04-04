479c044a7ae41b508dacc2df5f35df1aaff015ef
Marco 9726 <marcosarra9@gmail.com>
2023-04-04 11:32:42 +0200

    style  stripe success.blade

--- resources/views/admin/stripe/success.blade.php
+++ resources/views/admin/stripe/success.blade.php
@@ -1,11 +1,12 @@
 @extends('layouts.layout')
 
 @section('content')
-    <div class="container">
+    <div class="container w-75" id="successPayment">
         <div class="row mt-5">
             <div class="col-12 text-center">
                 <h1>Pagamento effettuato con successo</h1>
-                <a class="btn btn-success w-25" href="{{route('admin.dashboard')}}">Torna alla Dashboard</a>
+                {{-- <a class="btn btn-success w-25" href="{{route('admin.dashboard')}}">Torna alla Dashboard</a> --}}
+					 <img src="{{ URL('https://www.pikpng.com/pngl/b/428-4285487_vault-boy-thumbs-up-black-and-white-clipart.png')}}" alt="" class="w-50 mt-3">
             </div>
         </div>
     </div>
