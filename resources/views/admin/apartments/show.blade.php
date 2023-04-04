5960d04a66cc7c7a1d3f2b9e27d4074e67a971bc
Saraesp <saraesposito145@gmail.com>
2023-04-04 16:11:15 +0200

    edit show

--- resources/views/admin/apartments/show.blade.php
+++ resources/views/admin/apartments/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container pb-3">
         <div class="col-12 col-md-9 d-flex">
-            <a href="{{ route('admin.apartments.index') }}" role="button"><i class="fa-solid fa-circle-arrow-left fs-3 me-2 mt-2 btn-hover" style="color: #3a92d1"></i></a>
+            <a href="{{ route('admin.apartments.index') }}" role="button"><i class="fa-solid fa-circle-arrow-left fs-3 me-2 mt-2 btn-hover" style="color: #3FA9F5"></i></a>
             <h2 class="fw-bolder">{{ $apartment->descrizione ? $apartment->descrizione : 'Nome non specificato' }}</h2>
         </div>
 
@@ -59,48 +59,45 @@
                         <!-- Copertina appartamento -->
                         <div class="col-12 position-relative">
                             <div>
-                                <img src="{{($apartment->cover) ? asset('storage/' .$apartment->cover) : 'https://media.istockphoto.com/id/1147544807/it/vettoriale/la-commissione-per-la-immagine-di-anteprima-grafica-vettoriale.jpg?s=612x612&w=0&k=20&c=gsxHNYV71DzPuhyg-btvo-QhhTwWY0z4SGCSe44rvg4='}}" alt="" class="rounded w-100">
+                                <img src="{{($apartment->cover) ? asset('storage/' .$apartment->cover) : 'https://media.istockphoto.com/id/1147544807/it/vettoriale/la-commissione-per-la-immagine-di-anteprima-grafica-vettoriale.jpg?s=612x612&w=0&k=20&c=gsxHNYV71DzPuhyg-btvo-QhhTwWY0z4SGCSe44rvg4='}}" alt="" class="rounded w-100" style="max-height: 300px">
                             </div>
-                            <div class="row mt-2 me-3 position-absolute bottom-0 end-0">
-                                <div class="col-12 col-md-3 d-flex mb-2">
-                                    <a class="btn bg_color_yellow  me-1 text-white" href="{{ route('admin.apartments.edit', $apartment->slug) }}" role="button" title="Modifica il progetto"><i class="fa-solid fa-pen"></i></a>
-                                    <form action="{{ route('admin.apartments.destroy', $apartment->slug) }}" class="d-inline-block" method="POST">
-                                        @csrf
-                                        @method('DELETE')
-                                        <!-- Button trigger modal -->
-                                        <button type="button" class="btn text-white bg_color_red" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-                                            <i class="fa-solid fa-trash-can"></i>
-                                        </button>
-                                        <!-- Modal --> 
-                                        <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                                            <div class="modal-dialog">
-                                                <div class="modal-content">
-                                                <div class="modal-header">
-                                                    <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
-                                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                                </div>
-                                                <div class="modal-body">
-                                                    Sei sicuro di voler eliminare l'appartamento?
-                                                </div>
-                                                <div class="modal-footer">
-                                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
-                                                    <button class="btn btn-primary" type="submit">Si,elimina</button>
-                                                </div>
+                            <div class="row mt-2 me-3">
+                                <div class="col-12 col-md-3 mb-2 d-flex">
+                                    <h5 class="fw-bold mt-2 ms-4 position-absolute top-0 start-0" mb-2><span class="badge bg_color_light_blue"><i class="fa-solid fa-eye me-2"></i> {{count($apartment_views) > 0 ? count($apartment_views) : 'Nessun utente ha visualizzato il tuo apprtamento' }}</span></h5>
+                                    <div class="mb-4 me-4 position-absolute bottom-0 end-0">
+                                        <a class="btn bg_color_yellow  me-1 text-white" href="{{ route('admin.apartments.edit', $apartment->slug) }}" role="button" title="Modifica il progetto"><i class="fa-solid fa-pen"></i></a>
+                                        <form action="{{ route('admin.apartments.destroy', $apartment->slug) }}" class="d-inline-block" method="POST">
+                                            @csrf
+                                            @method('DELETE')
+                                            <!-- Button trigger modal -->
+                                            <button type="button" class="btn text-white bg_color_red" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                                                <i class="fa-solid fa-trash-can"></i>
+                                            </button>
+                                            <!-- Modal --> 
+                                            <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                                                <div class="modal-dialog">
+                                                    <div class="modal-content">
+                                                    <div class="modal-header">
+                                                        <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+                                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                    </div>
+                                                    <div class="modal-body">
+                                                        Sei sicuro di voler eliminare l'appartamento?
+                                                    </div>
+                                                    <div class="modal-footer">
+                                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
+                                                        <button class="btn btn-primary" type="submit">Si,elimina</button>
+                                                    </div>
+                                                    </div>
                                                 </div>
                                             </div>
-                                        </div>
-                                    </form>
+                                        </form>
+                                    </div>
                                 </div>
                             </div>
                         </div>
 
-                        <div class="row mt-2">  
-                            <div class="col-12 mb-2">
-                                <h5 class="fw-bold m-0" mb-2><span class="badge bg_color_light_blue"><i class="fa-solid fa-eye me-2"></i> {{count($apartment_views) > 0 ? count($apartment_views) : 'Nessun utente ha visualizzato il tuo apprtamento' }}</span></h5>
-                            </div>
-                        </div>
-                        <hr>
-                        <h5 class="fw-bold d-flex justify-content-between">Prezzo <span>&euro; {{ $apartment->prezzo ? $apartment->prezzo : 'Non specificato' }}<span class="fw-light fs-6"> / notte</span></span></h5>
+                        <h5 class="fw-bold mt-2 d-flex justify-content-between">Prezzo <span>&euro; {{ $apartment->prezzo ? $apartment->prezzo : 'Non specificato' }}<span class="fw-light fs-6"> / notte</span></span></h5>
                         <hr>
                         <!-- Servizi -->
                        <div class="col-12 col-md-6 mt-1">
