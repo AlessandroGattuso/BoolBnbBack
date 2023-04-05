2f6147a29cad262159c4e5a2ef700e0cf9c81037
micheleTummolo <tummolomichele02@gmail.com>
2023-04-05 20:15:51 +0200

    Added created_at and updated_at

--- app/Http/Controllers/StripeController.php
+++ app/Http/Controllers/StripeController.php
@@ -65,11 +65,14 @@ class StripeController extends Controller
             'data_inizio' => $data_inizio,
             'data_scadenza' => $data_scadenza,
             'apartment_id' => $apartment_id,
-            'sponsorship_id' => $sponsorship_id
+            'sponsorship_id' => $sponsorship_id,
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+
         );
-        /* dd($newSponsorship);
-        $apartment->sponsorships()->attach($newSponsorship); */
-        /* dd($apartment->sponsorships); */
+        /* dd($newSponsorship); */
+        $apartment->sponsorships()->attach($newSponsorship);
+        dd($apartment->sponsorships);
         
         return view('admin.stripe.success', compact('slug'));
     }
