d99cf3b198084ee7442e2fe5ea4cb8ff2b4c5b50
Marco 9726 <marcosarra9@gmail.com>
2023-04-07 19:49:45 +0200

    dates on messages

--- app/Http/Controllers/Admin/ApartmentController.php
+++ app/Http/Controllers/Admin/ApartmentController.php
@@ -27,8 +27,8 @@ class ApartmentController extends Controller
 		$userId = Auth::id();
 		$userName = Auth::user()->nome;
 		$apartments = Apartment::all()->where('user_id', $userId);
-	
-		return view('admin.dashboard', compact('apartments','userName'));
+
+		return view('admin.dashboard', compact('apartments', 'userName'));
 	}
 
 	/**
@@ -38,10 +38,10 @@ class ApartmentController extends Controller
 	 */
 	public function create()
 	{
-		$services = Service::all(); 
+		$services = Service::all();
 		$sponsorships = Sponsorship::all();
-		
-		return view('admin.Apartments.create', compact('services','sponsorships'));
+
+		return view('admin.Apartments.create', compact('services', 'sponsorships'));
 	}
 
 	/**
@@ -52,59 +52,57 @@ class ApartmentController extends Controller
 	 */
 	public function store(StoreApartmentRequest $request)
 	{
-		
+
 		$data = $request->validated();
-		if(!isset($data['visible'])){
+		if (!isset($data['visible'])) {
 			$data['visible'] = 0;
-		}
-		else{
+		} else {
 			$data['visible'] = 1;
 		}
-		
+
 		$data['slug'] = Apartment::generateSlug($request->descrizione);
 
 		$data['user_id'] =  Auth::id();
-		
+
 		$newPosition = new Position();
 
-		if($request->hasFile('cover')){
+		if ($request->hasFile('cover')) {
 			$path = Storage::disk('public')->put('apartment_images', $request->cover);
-			
+
 			$data['cover'] = $path;
 		}
 
 		$request->validate([
-			'indirizzo' => ['required','string', 'max:255'],
-			'N_civico' => ['required','numeric', 'max:255'],
-			'città' => ['required','string', 'max:50'],
+			'indirizzo' => ['required', 'string', 'max:255'],
+			'N_civico' => ['required', 'numeric', 'max:255'],
+			'città' => ['required', 'string', 'max:50'],
 			'Nazione' => ['required', 'string', 'max:20'],
 		]);
-		
+
 		$newPosition->indirizzo = $request->indirizzo;
 		$newPosition->N_civico = $request->N_civico;
 		$newPosition->città = $request->città;
 		$newPosition->Nazione = $request->Nazione;
 
-		$response = Http::get('https://api.tomtom.com/search/2/geocode/'.$newPosition->indirizzo.', '.$newPosition->città.', '.$newPosition->Nazione.'.json?key='.env('TOMTOM_KEY'));
+		$response = Http::get('https://api.tomtom.com/search/2/geocode/' . $newPosition->indirizzo . ', ' . $newPosition->città . ', ' . $newPosition->Nazione . '.json?key=' . env('TOMTOM_KEY'));
 		$jsonPosition = $response->json()['results'][0];
 
-		if($jsonPosition['position']['lat'] && $jsonPosition['position']['lon']){
+		if ($jsonPosition['position']['lat'] && $jsonPosition['position']['lon']) {
 			$newPosition->Latitudine = $jsonPosition['position']['lat'];
 			$newPosition->Longitudine = $jsonPosition['position']['lon'];
-		}
-		else{
+		} else {
 			return back()->with('error', 'Position not found');
 		}
-		
+
 		$apartment = Apartment::create($data);
-		
+
 		$newPosition->apartment_id = $apartment->id;
 
 		$apartment->position()->save($newPosition);
-		
-		if($request->has('services'))
+
+		if ($request->has('services'))
 			$apartment->services()->attach($request->services);
-		
+
 		$apartment_views = [];
 
 		return view('admin.apartments.show', compact('apartment', 'apartment_views'))->with('message', 'Hai aggiunto un nuovo appartamento con successo');
@@ -119,7 +117,7 @@ class ApartmentController extends Controller
 	public function show(Apartment $apartment)
 	{
 		$userId = Auth::id();
-		if($userId != $apartment->user_id){
+		if ($userId != $apartment->user_id) {
 
 			return redirect()->route('admin.dashboard')->with('warning', 'Non puoi visualizzare gli appartamenti di un altro utente');
 		}
@@ -128,14 +126,13 @@ class ApartmentController extends Controller
 		$apartment_views = [];
 		$views = View::all();
 
-		foreach($views as $view){
-			if($apartment->id == $view->apartment_id){
+		foreach ($views as $view) {
+			if ($apartment->id == $view->apartment_id) {
 				$apartment_views[] = $view;
 			}
 		}
 
 		return view('admin.apartments.show', compact('apartment', 'apartment_views'));
-
 	}
 
 	/**
@@ -147,12 +144,12 @@ class ApartmentController extends Controller
 	public function edit(Apartment $apartment)
 	{
 		$userId = Auth::id();
-		if($userId != $apartment->user_id){
+		if ($userId != $apartment->user_id) {
 
 			return redirect()->route('admin.dashboard')->with('warning', 'Non puoi modificare gli appartamenti di un altro utente');
 		}
-		
-		$services = Service::all(); 
+
+		$services = Service::all();
 		return view('admin.apartments.edit', compact('apartment', 'services'));
 	}
 
@@ -167,20 +164,19 @@ class ApartmentController extends Controller
 	{
 
 		$userId = Auth::id();
-		if($userId != $apartment->user_id){
+		if ($userId != $apartment->user_id) {
 			return redirect()->route('admin.dashboard')->with('warning', 'Non puoi modificare gli appartamenti di un altro utente');
 		}
 
 		$data = $request->validated();
-		
-		if(!isset($data['visible'])){
+
+		if (!isset($data['visible'])) {
 			$data['visible'] = 0;
-		}
-		else{
+		} else {
 			$data['visible'] = 1;
 		}
 		//$position = Position::all()->where('apartment_id', $apartment->id);
-	
+
 		// $request->validate([
 		// 	'indirizzo' => ['required','string', 'max:255'],
 		// 	'N_civico' => ['required','numeric', 'max:255'],
@@ -188,43 +184,42 @@ class ApartmentController extends Controller
 		// 	'Nazione' => ['required', 'string', 'max:20'],
 		// ]);
 
-		$response = Http::get('https://api.tomtom.com/search/2/geocode/'.$request->indirizzo.', '.$request->città.', '.$request->Nazione.'.json?key='.env('TOMTOM_KEY'));
+		$response = Http::get('https://api.tomtom.com/search/2/geocode/' . $request->indirizzo . ', ' . $request->città . ', ' . $request->Nazione . '.json?key=' . env('TOMTOM_KEY'));
 		$jsonPosition = $response->json()['results'][0];
 
-		if(!($jsonPosition['position']['lat'] && $jsonPosition['position']['lon'])){
+		if (!($jsonPosition['position']['lat'] && $jsonPosition['position']['lon'])) {
 			return back()->with('error', 'Position not found');
 		}
 
-		
+
 		$data['slug'] = apartment::generateSlug($request->descrizione);
-		
-		if($request->hasFile('cover')){
-			
-					if($apartment->cover)
-							Storage::delete($apartment->cover);  
-			
-			
-					$path = Storage::disk('public')->put('apartment_images', $request->cover); 
-					$data['cover'] = $path;
-			
-			}
-		
-			$apartment->update($data);
 
-			$apartment->position->indirizzo = $request['indirizzo'];
-			$apartment->position->N_civico = $request['N_civico'];
-			$apartment->position->città = $request['città'];
-			$apartment->position->Nazione = $request['Nazione'];
-			$apartment->position->Longitudine = $jsonPosition['position']['lon'];
-			$apartment->position->Latitudine = $jsonPosition['position']['lat'];
+		if ($request->hasFile('cover')) {
 
-			$apartment->push();
+			if ($apartment->cover)
+				Storage::delete($apartment->cover);
 
-		if($request->has('services'))
-				$apartment->services()->sync($request->services);
+
+			$path = Storage::disk('public')->put('apartment_images', $request->cover);
+			$data['cover'] = $path;
+		}
+
+		$apartment->update($data);
+
+		$apartment->position->indirizzo = $request['indirizzo'];
+		$apartment->position->N_civico = $request['N_civico'];
+		$apartment->position->città = $request['città'];
+		$apartment->position->Nazione = $request['Nazione'];
+		$apartment->position->Longitudine = $jsonPosition['position']['lon'];
+		$apartment->position->Latitudine = $jsonPosition['position']['lat'];
+
+		$apartment->push();
+
+		if ($request->has('services'))
+			$apartment->services()->sync($request->services);
 		else
-				$apartment->services()->detach();
-			
+			$apartment->services()->detach();
+
 
 		return redirect()->route('admin.apartments.index')->with('message', "Le modifiche all'appartamento sono state salvate correttamente");
 	}
@@ -238,11 +233,11 @@ class ApartmentController extends Controller
 	public function destroy(Apartment $apartment)
 	{
 		$userId = Auth::id();
-		if($userId != $apartment->user_id){
+		if ($userId != $apartment->user_id) {
 			return redirect()->route('admin.dashboard')->with('warning', 'Non puoi modificare gli appartamenti di un altro utente');
 		}
 
 		$apartment->delete();
-    	return redirect()->route('admin.apartments.index')->with('message',"L'appartmanento è rimosso correttamente");
+		return redirect()->route('admin.apartments.index')->with('message', "L'appartmanento è rimosso correttamente");
 	}
 }
