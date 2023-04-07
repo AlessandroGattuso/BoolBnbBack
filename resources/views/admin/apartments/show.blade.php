9afe3a39a228260ff053f99c6619a68c335e06c0
roberto.boston <roberto.bozzo97@tiscali.it>
2023-04-07 19:05:55 +0200

    modify style delete modal

--- resources/views/admin/apartments/show.blade.php
+++ resources/views/admin/apartments/show.blade.php
@@ -95,15 +95,14 @@
                                                 <div class="modal-dialog">
                                                     <div class="modal-content">
                                                     <div class="modal-header">
-                                                        <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
                                                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                                     </div>
                                                     <div class="modal-body">
                                                         Sei sicuro di voler eliminare l'appartamento?
                                                     </div>
                                                     <div class="modal-footer">
-                                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
-                                                        <button class="btn btn-primary" type="submit">Si,elimina</button>
+                                                        <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Annulla</button>
+                                                        <button class="btn btn-danger" type="submit">Si, elimina</button>
                                                     </div>
                                                     </div>
                                                 </div>
