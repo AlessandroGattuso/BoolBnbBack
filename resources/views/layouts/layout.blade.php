8643d0f85a34e0eb0fa165b77c444d8e85208a13
micheleTummolo <tummolomichele02@gmail.com>
2023-04-08 10:54:39 +0200

    Fixed title and favicon

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -8,7 +8,8 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+	<link rel="shortcut icon" href="{{ URL::asset('/img/bnb-logo.png')}}" type="image/x-icon">
+    <title>Boolbnb</title>
 
 
     <!-- Fonts -->
